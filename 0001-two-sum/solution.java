7b50c7726236fae3cd15a976b11e5860adf31397
Maaz N. <maaz73209@gmail.com>
2026-01-06 12:15:23 +0000

    Sync LeetCode submission - Two Sum - Runtime - 33 ms (41.45%), Memory - 47.1 MB (57.49%)

---
+++ 0001-two-sum/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        for(int i=0;i<nums.length;i++){
+            int k=target-nums[i];
+            for(int j=i+1;j<nums.length;j++){
+              if(nums[j]==k){
+                    return new int[] { i, j };
+                }
+
+            }
+
+            }
+
+        
+        return null;
+    }
+}
