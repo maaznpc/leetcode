e0d663f5ad0cbf5691693ff38b93b71b915b9d71
Maaz N. <maaz73209@gmail.com>
2026-06-21 19:34:48 +0000

    Sync LeetCode submission - Average Selling Price - Runtime - 222 ms (30.74%), Memory - 0.0B (100.00%)

---
+++ 1390-average-selling-price/solution.sql
@@ -0,0 +1,10 @@
+select p.product_id ,  
+    round(coalesce
+            (sum(price*units):: numeric /sum(units) , 0 )
+            , 2 )
+             as average_price
+from prices p 
+    left join unitssold u 
+    on p.product_id = u.product_id
+    and u.purchase_date between  p.start_date and p.end_date
+group by p.product_id
