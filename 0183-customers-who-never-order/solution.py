f763efbe7df8e21e387491c27f56413cf1eb555e
Maaz N. <maaz73209@gmail.com>
2026-07-06 12:14:35 +0000

    Sync LeetCode submission - Customers Who Never Order - Runtime - 309 ms (18.30%), Memory - 68 MB (35.16%)

---
+++ 0183-customers-who-never-order/solution.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    merged=customers.merge(
+        orders ,
+        left_on='id',
+        right_on='customerId',
+        how = 'left'
+    )
+    return merged[merged['customerId'].isna()][['name']].rename(columns={'name':'customers'})
