1556e36e7c9c10c22838bdbe8b7dcfb3a8170842
Maaz N. <maaz73209@gmail.com>
2026-07-06 17:50:54 +0000

    Sync LeetCode submission - Calculate Special Bonus - Runtime - 359 ms (10.02%), Memory - 68.1 MB (12.36%)

---
+++ 2024-calculate-special-bonus/solution.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = 0
+    mask = (employees['employee_id'] % 2 != 0) & (~employees['name'].str.startswith('M'))
+    employees.loc[mask, 'bonus'] = employees.loc[mask, 'salary']
+    return employees[['employee_id','bonus']].sort_values('employee_id')
+
