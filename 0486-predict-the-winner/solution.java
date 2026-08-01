d824549fe84598571018cd65973f8096c64560fd
Maaz N. <maaz73209@gmail.com>
2026-08-01 17:50:56 +0000

    Sync LeetCode submission - Predict the Winner - Runtime - 0 ms (100.00%), Memory - 42.8 MB (57.54%)

---
+++ 0486-predict-the-winner/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n= nums.length;
+        int [] dp =new int[n];
+        for(int i=n-1;i>=0;i--)
+        {
+            dp[i] =nums[i];
+            for(int j=i+1;j<n;j++){
+                int tl=nums[i] - dp[j];
+                int tr=nums[j] - dp[j-1];
+                dp[j]= Math.max(tl,tr);
+            }
+        }
+        return dp[n-1] >=0;
+    }
+}
